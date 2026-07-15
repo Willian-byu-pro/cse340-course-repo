3c52ed5f3379d73e13bcddf61a2101c219945c57
Willian-byu-pro <willianramosdida@gmail.com>
2026-07-14 21:28:22 -0300

    Up data

--- src/models/project.js
+++ src/models/project.js
@@ -2,10 +2,12 @@ import db from './db.js'
 
 const getAllProjects = async() => {
     const query = `
-         SELECT sp.date, sp.title, o.name AS organization_name
+         SELECT
+            TO_CHAR(sp.date, 'YYYY-MM-DD') AS date,
+            sp.title,
+            o.name AS organization_name
          FROM service_project sp
-         JOIN organization o 
-            ON sp.organization_id = o.organization_id
+         JOIN organization o ON sp.organization_id = o.organization_id
          ORDER BY sp.date;`;
 
     const result = await db.query(query);
